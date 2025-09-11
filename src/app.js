7989c79477698fc7b95efcc376256f363515ac24
John Doe <john.doe@contoso.com>
2025-09-11 18:01:30 -0500

    actualizando codigo

--- src/app.js
+++ src/app.js
@@ -1,24 +1,25 @@
 import express from "express";
- 
-const app = express();
-const PORT = process.env.PORT || 3000;
- 
+
+const app = express(); // ← primero debes inicializar express
+const port = process.env.PORT || 3000;
+
+// Rutas
 app.get("/", (_req, res) => {
   res.json({
     message: "Hello Cloud ☁️",
     docs: ["/health", "/version"]
   });
 });
- 
+
 app.get("/health", (_req, res) => {
   res.json({ status: "ok", uptime: process.uptime() });
 });
- 
+
 app.get("/version", (_req, res) => {
   res.json({ name: "hello-cloud", version: "1.0.0" });
 });
- 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+
+// Iniciar servidor
+app.listen(port, () => {
+  console.log(`🚀 Server running on http://localhost:${port}`);
 });
- 
